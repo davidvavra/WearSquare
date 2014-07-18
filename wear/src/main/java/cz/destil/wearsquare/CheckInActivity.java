49b10d916b4e650dc654feb69c38c9e388cbbc33
David Vavra <me@destil.cz>
2014-07-18 15:07:07 +0200

    working hello world

---
+++ wear/src/main/java/cz/destil/wearsquare/CheckInActivity.java
@@ -0,0 +1,24 @@
+package cz.destil.wearsquare;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class CheckInActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
