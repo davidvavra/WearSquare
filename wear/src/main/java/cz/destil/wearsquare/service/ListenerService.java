5ce56e6385a00e9091890411b412114b1bc051d9
David Vavra <me@destil.cz>
2014-08-07 20:49:15 +0200

    bugfix for https://github.com/Mariuxtheone/Teleport/issues/3

--- wear/src/main/java/cz/destil/wearsquare/service/ListenerService.java
+++ wear/src/main/java/cz/destil/wearsquare/service/ListenerService.java
@@ -2,8 +2,12 @@ package cz.destil.wearsquare.service;
 
 import android.graphics.Bitmap;
 
+import com.google.android.gms.common.data.FreezableUtils;
 import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
 import com.mariux.teleport.lib.TeleportService;
 
 import java.util.ArrayList;
@@ -21,78 +25,51 @@ import cz.destil.wearsquare.util.DebugLog;
 
 public class ListenerService extends TeleportService {
 
+    /**
+     * Overriding as workaround to:
+     * https://github.com/Mariuxtheone/Teleport/issues/3
+     */
     @Override
-    public void onCreate() {
-        super.onCreate();
-        setOnSyncDataItemTask(new SyncDataTask());
-    }
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        final List<DataEvent> events = FreezableUtils.freezeIterable(dataEvents);
+        for (DataEvent event : events) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
+                DataMap data = dataMapItem.getDataMap();
+                if (data.containsKey("error_message")) {
+                    DebugLog.d("error");
+                    App.bus().post(new ErrorEvent(data.getString("error_message")));
+                } else if (data.containsKey("check_in_venues")) {
+                    DebugLog.d("check in");
+                    processCheckInList(data);
+                } else if (data.containsKey("explore_venues")) {
+                    DebugLog.d("explore");
+                    processExploreList(data);
+                } else if (data.containsKey("image_url")) {
+                    new ProcessImageTask(data).start();
+                }
 
-    class SyncDataTask extends OnSyncDataItemTask {
-        @Override
-        protected void onPostExecute(DataMap data) {
-            if (data.containsKey("error_message")) {
-                DebugLog.d("error");
-                App.bus().post(new ErrorEvent(data.getString("error_message")));
-            } else if (data.containsKey("check_in_venues")) {
-                DebugLog.d("check in");
-                new CheckInVenuesTask(data).start();
-            } else if (data.containsKey("explore_venues")) {
-                DebugLog.d("explore");
-                new ExploreVenuesTask(data).start();
-            } else if (data.containsKey("image_url")) {
-                new ProcessImageTask(data).start();
             }
-            setOnSyncDataItemTask(new SyncDataTask());
-        }
-    }
-
-    class CheckInVenuesTask extends BaseAsyncTask {
-
-        private DataMap data;
-
-        CheckInVenuesTask(DataMap data) {
-            this.data = data;
-        }
-
-        @Override
-        public void inBackground() {
-            List<CheckInAdapter.Venue> venues = new ArrayList<CheckInAdapter.Venue>();
-            ArrayList<DataMap> dataVenues = data.getDataMapArrayList("check_in_venues");
-            for (DataMap dataVenue : dataVenues) {
-                venues.add(new CheckInAdapter.Venue(dataVenue.getString("id"), dataVenue.getString("name"), dataVenue.getString("image_url")));
-            }
-            App.bus().post(new CheckInVenueListEvent(venues));
-        }
-
-        @Override
-        public void postExecute() {
-
         }
     }
 
-    class ExploreVenuesTask extends BaseAsyncTask {
-
-        private DataMap data;
-
-        ExploreVenuesTask(DataMap data) {
-            this.data = data;
+    private void processCheckInList(DataMap data) {
+        List<CheckInAdapter.Venue> venues = new ArrayList<CheckInAdapter.Venue>();
+        ArrayList<DataMap> dataVenues = data.getDataMapArrayList("check_in_venues");
+        for (DataMap dataVenue : dataVenues) {
+            venues.add(new CheckInAdapter.Venue(dataVenue.getString("id"), dataVenue.getString("name"), dataVenue.getString("image_url")));
         }
+        App.bus().post(new CheckInVenueListEvent(venues));
+    }
 
-        @Override
-        public void inBackground() {
-            List<ExploreAdapter.Venue> venues = new ArrayList<ExploreAdapter.Venue>();
-            ArrayList<DataMap> dataVenues = data.getDataMapArrayList("explore_venues");
-            for (DataMap dataVenue : dataVenues) {
-                venues.add(new ExploreAdapter.Venue(dataVenue.getString("id"), dataVenue.getString("name"), dataVenue.getString("tip"),
-                        dataVenue.getDouble("latitude"), dataVenue.getDouble("longitude"), dataVenue.getString("image_url")));
-            }
-            App.bus().post(new ExploreVenueListEvent(venues));
+    private void processExploreList(DataMap data) {
+        List<ExploreAdapter.Venue> venues = new ArrayList<ExploreAdapter.Venue>();
+        ArrayList<DataMap> dataVenues = data.getDataMapArrayList("explore_venues");
+        for (DataMap dataVenue : dataVenues) {
+            venues.add(new ExploreAdapter.Venue(dataVenue.getString("id"), dataVenue.getString("name"), dataVenue.getString("tip"),
+                    dataVenue.getDouble("latitude"), dataVenue.getDouble("longitude"), dataVenue.getString("image_url")));
         }
-
-        @Override
-        public void postExecute() {
-
-        }
+        App.bus().post(new ExploreVenueListEvent(venues));
     }
 
     class ProcessImageTask extends BaseAsyncTask {
